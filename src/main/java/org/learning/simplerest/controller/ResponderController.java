82477330b8423e7a1c63b37d629751011fe69910
otabekgeek <otabekgeek@gmail.com>
2025-06-25 17:05:27 +0500

    debug

--- src/main/java/org/learning/simplerest/controller/ResponderController.java
+++ src/main/java/org/learning/simplerest/controller/ResponderController.java
@@ -3,13 +3,13 @@ package org.learning.simplerest.controller;
 import lombok.RequiredArgsConstructor;
 import org.learning.simplerest.service.ResponderService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api")
-@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api")
 public class ResponderController {
-    private ResponderService responderService;
 
     @GetMapping("/getName")
     public String getResponse(@RequestParam String name) {
